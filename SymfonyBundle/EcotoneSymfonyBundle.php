eb63afef9c0546f6972e7ed8d2c2d03baded5d77
Dariusz <dgafka.mail@gmail.com>
2023-05-29 18:11:32 +0000

    feat(Symfony): boot messaging system on service injection (#121)

--- SymfonyBundle/EcotoneSymfonyBundle.php
+++ SymfonyBundle/EcotoneSymfonyBundle.php
@@ -3,15 +3,10 @@
 namespace Ecotone\SymfonyBundle;
 
 use Ecotone\Messaging\Config\ConfiguredMessagingSystem;
-use Ecotone\Messaging\Handler\ExpressionEvaluationService;
-use Ecotone\Messaging\Handler\ReferenceSearchService;
-use Ecotone\Messaging\Handler\SymfonyExpressionEvaluationAdapter;
 use Ecotone\SymfonyBundle\DepedencyInjection\Compiler\EcotoneCompilerPass;
 use Ecotone\SymfonyBundle\DepedencyInjection\EcotoneExtension;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
-use Symfony\Component\DependencyInjection\Definition;
 use Symfony\Component\DependencyInjection\Extension\ExtensionInterface;
-use Symfony\Component\ExpressionLanguage\ExpressionLanguage;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 /**
@@ -30,44 +25,6 @@ class EcotoneSymfonyBundle extends Bundle
     public function build(ContainerBuilder $container)
     {
         $container->addCompilerPass(new EcotoneCompilerPass());
-
-        $this->setUpExpressionLanguage($container);
-
-        $definition = new Definition();
-        $definition->setClass(ConfiguredMessagingSystem::class);
-        $definition->setSynthetic(true);
-        $definition->setPublic(true);
-        $container->setDefinition(ConfiguredMessagingSystem::class, $definition);
-    }
-
-    /**
-     * @param ContainerBuilder $container
-     * @return void
-     */
-    private function setUpExpressionLanguage(ContainerBuilder $container): void
-    {
-        $expressionLanguageAdapter = ExpressionEvaluationService::REFERENCE . '_adapter';
-        $definition = new Definition();
-        $definition->setClass(ExpressionLanguage::class);
-
-        $container->setDefinition($expressionLanguageAdapter, $definition);
-
-        $definition = new Definition();
-        $definition->setClass(SymfonyExpressionEvaluationAdapter::class);
-        $definition->setFactory([SymfonyExpressionEvaluationAdapter::class, 'create']);
-        $definition->setPublic(true);
-        $container->setDefinition(ExpressionEvaluationService::REFERENCE, $definition);
-    }
-
-    public function boot()
-    {
-        $configuration = EcotoneCompilerPass::getMessagingConfiguration($this->container, true);
-
-        $referenceSearchService = $this->container->get(ReferenceSearchService::class);
-        $messagingSystem = $configuration->buildMessagingSystemFromConfiguration($referenceSearchService);
-        $referenceSearchService->setConfiguredMessagingSystem($messagingSystem);
-
-        $this->container->set(ConfiguredMessagingSystem::class, $messagingSystem);
     }
 
     public function getContainerExtension(): ?ExtensionInterface
