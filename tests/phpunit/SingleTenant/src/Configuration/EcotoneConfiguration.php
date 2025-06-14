45dd22a7ac3691ff1e6ead3eb2fd2dbab6a37dd8
Ecotone <ecotoneframework@gmail.com>
2025-06-14 14:17:49 +0000

    Command Bus Error Channel and Instant Retries (#488)

--- tests/phpunit/SingleTenant/src/Configuration/EcotoneConfiguration.php
+++ tests/phpunit/SingleTenant/src/Configuration/EcotoneConfiguration.php
@@ -5,9 +5,11 @@ declare(strict_types=1);
 namespace Symfony\App\SingleTenant\Configuration;
 
 use Ecotone\Dbal\Configuration\DbalConfiguration;
-use Ecotone\Dbal\DbalBackedMessageChannelBuilder;
+use Ecotone\Dbal\Recoverability\DbalDeadLetterBuilder;
 use Ecotone\Messaging\Attribute\ServiceContext;
+use Ecotone\Messaging\Endpoint\PollingMetadata;
 use Ecotone\SymfonyBundle\Config\SymfonyConnectionReference;
+use Ecotone\SymfonyBundle\Messenger\SymfonyMessengerMessageChannelBuilder;
 
 /**
  * licence Apache-2.0
@@ -24,12 +26,22 @@ final class EcotoneConfiguration
     public function tenantAConnection(): DbalConfiguration
     {
         return DbalConfiguration::createWithDefaults()
-                 ->withDoctrineORMRepositories(true);
+                 ->withDoctrineORMRepositories(true)
+                 ->withDeadLetter(true);
     }
 
     #[ServiceContext]
-    public function databaseChannel(): DbalBackedMessageChannelBuilder
+    public function databaseChannel(): SymfonyMessengerMessageChannelBuilder
     {
-        return DbalBackedMessageChannelBuilder::create('notifications');
+        return SymfonyMessengerMessageChannelBuilder::create('notifications');
+    }
+
+    #[ServiceContext]
+    public function notificationPollingMetadata(): PollingMetadata
+    {
+        return PollingMetadata::create('notifications')
+            ->setExecutionTimeLimitInMilliseconds(1000)
+            ->setHandledMessageLimit(10)
+            ->setErrorChannelName(DbalDeadLetterBuilder::STORE_CHANNEL);
     }
 }
